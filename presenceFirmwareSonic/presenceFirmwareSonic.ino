c429e889515c67696e9973af68fe3705b9057db7
munhra <rafael.munhoz@gmail.com>
2017-08-28 11:12:53 -0300

    Improved firmware to send debug information

--- presenceFirmwareSonic/presenceFirmwareSonic.ino
+++ presenceFirmwareSonic/presenceFirmwareSonic.ino
@@ -7,19 +7,52 @@ const char* ssid     = "FollowMe-Pi3";
 const char* password = "FollowMeRadio";
 const char* host = "192.168.42.1";
 
+#define DEBUG true
+
 //sensor sonic
 #define TRIGGER 12
 #define ECHO    13
-//#define WALL_DISTANCE 140 //Kitchen
-#define WALL_DISTANCE 90 //bedroom
+
+//#define WALL_DISTANCE_LOW 150 //Kitchen 
+//#define WALL_DISTANCE_HIGH 199 //Kitchen 
+
+//#define WALL_DISTANCE 90 //bedroom
+//#define WALL_DISTANCE_LOW 89 //bedroom
+//#define WALL_DISTANCE_HIGH 98 //bedroom
+
+//define WALL_DISTANCE 110 //livingroom left
+//#define WALL_DISTANCE_LOW 170 //livingroom right was 145
+//#define WALL_DISTANCE_HIGH 175 //livingroom right was 145
+
+//define WALL_DISTANCE 110 //livingroom right
+#define WALL_DISTANCE_LOW 170 //livingroom right was 145
+#define WALL_DISTANCE_HIGH 180 //livingroom right was 145
+
+//old firmware working
 //#define WALL_DISTANCE 60 //bathroom
-//#define WALL_DISTANCE 120 //livingroom
-#define WALL_ERROR_ADJUST 30
+
+//#define WALL_ERROR_ADJUST 150 //delay removal test
+//#define FOTA_HOST_NAME "FutureHousePresenceBedroom" //192.168.42.19
+#define FOTA_HOST_NAME "FutureHousePresenceLivingRight"
+//#define FOTA_HOST_NAME "FutureHousePresenceLivingLeft"
+//#define FOTA_HOST_NAME "FutureHousePresenceKitchen"
+//#define FOTA_HOST_NAME "FutureHousePresenceBathroom"
 
 boolean sendPresenceDetected= true;
 boolean sendPresenceNotDetected = true;
 
-String roomName = "bedroom";
+//String roomName = "kitchen";
+//String roomName = "bedroom";
+String roomName = "livingroom";
+//String roomName = "bathroom";
+
+//Calibration measures
+int arraySize = 100;
+int standBydistanceArray[100];
+float standByAverageDistance = 0;
+int standByMaxDistance = 0;
+int standByMinDistance = 100000;
+int standByDistanceIndex = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -28,6 +61,7 @@ void setup() {
   sendRegister();
   setupFOTA();
   setupSonic();
+  resetStandByMeasures();
 }
 
 void loop() {
@@ -60,7 +94,7 @@ void setupSonic() {
 }
 
 void setupFOTA() {
-  ArduinoOTA.setHostname("munhraESP8266");
+  ArduinoOTA.setHostname(FOTA_HOST_NAME);
   ArduinoOTA.onStart([]() {
     Serial.println("Start");
   });
@@ -98,11 +132,13 @@ void controlSimpleSonicState() {
   distance = (duration/2) / 29.1;
 
   Serial.println(distance);
-  
-  if ((distance < WALL_DISTANCE) || (distance > WALL_DISTANCE + WALL_ERROR_ADJUST)){
-  
+  registerStandByDistance(distance);
+
+  if ((distance < WALL_DISTANCE_LOW) || (distance > WALL_DISTANCE_HIGH)){
+    
     if (sendPresenceDetected) {
       Serial.println("send post detected");
+      calculateStandByMeasures();
       sendDetectionPost("1");
       sendPresenceDetected = false;
       sendPresenceNotDetected = true;
@@ -141,6 +177,25 @@ void sendRegister()
   Serial.println("closing connection");
 }
 
+void sendDebugPost(String detected) {
+  Serial.print("connecting to ");
+  Serial.println(host);
+  WiFiClient client;
+  const int httpPort = 3000;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+  }
+  String url = "/api/sensor/debug?roomName="+roomName+"&mac="+getMacAddress()+"&ip="+ipToString(WiFi.localIP())+"&presence="+detected+
+               "&maxdistance="+String(standByMaxDistance)+
+               "&mindistance="+String(standByMinDistance)+
+               "&averagedistance="+String(standByAverageDistance);            
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+}
+
 void sendDetectionPost(String detected) {
   Serial.print("connecting to ");
   Serial.println(host);
@@ -160,6 +215,56 @@ void sendDetectionPost(String detected) {
                
   Serial.println();
   Serial.println("closing connection");
+}
+
+//Measures
+
+void resetStandByMeasures() {
+  for (int i = 0 ; i < arraySize; i++) { //sizeof(standBydistanceArray)
+    standBydistanceArray[i] = 0;
+  }
+  standByMaxDistance = 0;
+  standByMinDistance = 100000;
+  standByDistanceIndex = 0;
+  standByAverageDistance = 0;
+}
+
+void registerStandByDistance(int distance) {
+  if (standBydistanceArray[arraySize - 1] != 0) {
+    calculateStandByMeasures();
+    if (DEBUG) {
+      sendDebugPost("0");  
+    }
+    resetStandByMeasures();
+  }else{
+    standBydistanceArray[standByDistanceIndex] = distance;
+    standByDistanceIndex++;
+  }
+}
+
+void calculateStandByMeasures()
+{
+    Serial.println("calculateStandByMeasures");
+    int nonZeroesPositionCounter = 1;
+    int totalDistance = 0;
+    for (int i = 0 ; i < arraySize; i++) {
+      if (!standBydistanceArray[i] == 0) {
+        totalDistance = totalDistance + standBydistanceArray[i];
+        nonZeroesPositionCounter++;
+        if (standByMinDistance > standBydistanceArray[i]){
+          standByMinDistance = standBydistanceArray[i];
+        }
+        if (standByMaxDistance < standBydistanceArray[i]){
+          standByMaxDistance = standBydistanceArray[i];
+        }
+      }
+    }
+    
+    standByAverageDistance =  totalDistance / nonZeroesPositionCounter;    
+  
+    Serial.println("calculateStandByMeasures last measures maxdistance "+String(standByMaxDistance));
+    Serial.println("calculateStandByMeasures last measures mindistance "+String(standByMinDistance));
+    Serial.println("calculateStandByMeasures last measures average "+String(standByAverageDistance,3));
 }
 
 String ipToString(IPAddress ip){
